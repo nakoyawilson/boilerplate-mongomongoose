35d34f1e38d739d00fe6c1ba00aa29e6849b3d5b
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-11-16 16:56:48 -0400

    Modify the findPeopleByName function to find all the people having a given name

--- myApp.js
+++ myApp.js
@@ -39,8 +39,12 @@ const createManyPeople = (arrayOfPeople, done) => {
   })
 };
 
+const personName = "John Doe";
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  })
 };
 
 const findOneByFood = (food, done) => {
