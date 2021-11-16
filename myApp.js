7e657680ce89570841918b284f68f8933c92ca9a
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-11-16 15:32:03 -0400

    Create a person schema called personSchema and a model called Person from the personSchema

--- myApp.js
+++ myApp.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
+const { Schema } = require('mongoose');
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+})
+
+let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
