e5634cd0ef57dbb90d5924b52fcd6e48407f904a
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-11-16 17:20:59 -0400

    Modify the findPersonById to find the only person having a given _id

--- myApp.js
+++ myApp.js
@@ -51,12 +51,15 @@ const food = "donuts";
 const findOneByFood = (food, done) => {
   Person.findOne({ favoriteFoods: food }, (err, data) => {
     if (err) return done(err);
-    done(null, data)
+    done(null, data);
   })
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
